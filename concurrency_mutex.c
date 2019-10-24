28cb4acda31feb12e247118be315af7dc7a1d19b
Jizhou-Che <che_mail@icloud.com>
2019-10-24 02:18:57 +0100

    Semaphores tested.

--- concurrency_mutex.c
+++ concurrency_mutex.c
@@ -6,7 +6,7 @@
 #include <pthread.h>
 
 // Set each thread to increment the counter 50000000 times.
-#define number_of_increments 20000000
+#define number_of_increments 50000000
 
 // The mutex lock.
 pthread_mutex_t lock;
@@ -28,24 +28,18 @@ void * increment() {
 }
 
 int main() {
-	// Create five threads.
-	pthread_t tID1, tID2, tID3, tID4, tID5;
+	// Create two threads.
+	pthread_t tID1, tID2;
 	// Initialise mutex before creating threads.
 	pthread_mutex_init(&lock, NULL);
 	pthread_create(&tID1, NULL, increment, NULL);
 	pthread_create(&tID2, NULL, increment, NULL);
-	pthread_create(&tID3, NULL, increment, NULL);
-	pthread_create(&tID4, NULL, increment, NULL);
-	pthread_create(&tID5, NULL, increment, NULL);
 	
 	// Tell the main thread to wait for all threads to finish.
 	pthread_join(tID1, NULL);
 	pthread_join(tID2, NULL);
-	pthread_join(tID3, NULL);
-	pthread_join(tID4, NULL);
-	pthread_join(tID5, NULL);
 	
-	// Counter should be 100000000 but is actually much smaller.
+	// Print the value of the counter.
 	printf("The value of counter is: %d.\n", counter);
 	
 	return 0;
---
+++ concurrency_semaphore.c
@@ -0,0 +1,69 @@
+// An approach to mutual exclusion using semaphore.
+// The two threads now increment the counter to 100000000.
+// Note that this implementation is even less efficient than the mutex implementation.
+// This is because the enforcement of massive context switching generates even larger overhead than busy waiting.
+// A much more efficient version of the increment function is implemented in the comments.
+// This is achieved by performing less number of synchronisations and context switches.
+
+#include <stdio.h>
+#include <pthread.h>
+#include <semaphore.h>
+
+// Set each thread to increment the counter 50000000 times.
+#define number_of_increments 50000000
+
+// The semaphore.
+sem_t s;
+
+// The shared counter.
+int counter = 0;
+
+// Function to carry out inside each thread.
+void * increment() {
+	for (int i = 0; i < number_of_increments; i++) {
+		// Atomic function to wait a semaphore.
+		sem_wait(&s);
+		// Critical section.
+		counter++;
+		// Atomic function to post a semaphore.
+		sem_post(&s);
+	}
+	pthread_exit(NULL);
+}
+/*
+void * increment() {
+	int temp = 0;
+	for (int i = 0; i < number_of_increments; i++) {
+		temp++;
+		if (temp == 5000) {
+			// Atomic function to wait a semaphore.
+			sem_wait(&s);
+			// Critical section.
+			counter += temp;
+			// Atomic function to post a semaphore.
+			sem_post(&s);
+			temp = 0;
+		}
+	}
+	pthread_exit(NULL);
+}
+*/
+
+int main() {
+	// Create two threads.
+	pthread_t tID1, tID2;
+	// Initialise semaphore before creating threads.
+	// Initial internal value of semaphore is set to 1.
+	sem_init(&s, 0, 1);
+	pthread_create(&tID1, NULL, increment, NULL);
+	pthread_create(&tID2, NULL, increment, NULL);
+	
+	// Tell the main thread to wait for all threads to finish.
+	pthread_join(tID1, NULL);
+	pthread_join(tID2, NULL);
+	
+	// Print the value of the counter.
+	printf("The value of counter is: %d.\n", counter);
+	
+	return 0;
+}
